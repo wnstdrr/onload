d3578a3ac5948b763b1519cb79cf79c33a750883
wnstdrr <xandertedder7@gmail.com>
2022-05-31 20:13:50 -0400

    constants BAR, CHECK, ARROW renamed

--- include/colourbar/colourbar.c
+++ include/colourbar/colourbar.c
@@ -3,23 +3,23 @@
 #include <stddef.h>
 
 /* Determine how to output the bar array */
-const char *BAR_ARRAY[MAX_COLOUR_SIZE] = {
-        DRK_BAR,           BAR, RESET,
-        RED_LGHT_BAR,      BAR, RESET,
-        GREEN_DRK_BAR,     BAR, RESET,
-        YELLOW_DRK_BAR,    BAR, RESET,
-        BLUE_DRK_BAR,      BAR, RESET,
-        MAG_DRK_BAR,       BAR, RESET,
-        CYAN_DRK_BAR,      BAR, RESET,
-        WHITE_LGHT_BAR,    BAR, RESET,
-        LGHT_BAR,          BAR, RESET,
-        RED_DRK_BAR,       BAR, RESET,
-        GREEN_LGHT_BAR,    BAR, RESET,
-        YELLOW_LGHT_BAR,   BAR, RESET,
-        BLUE_LGHT_BAR,     BAR, RESET,
-        MAG_LGHT_BAR,      BAR, RESET,
-        CYAN_LGHT_BAR,     BAR, RESET,
-        WHITE_DRK_BAR,     BAR, RESET
+const char *BLOCK_ARRAY[MAX_COLOUR_SIZE] = {
+        DRK_BLOCK,           BLOCK, RESET,
+        RED_LGHT_BLOCK,      BLOCK, RESET,
+        GREEN_DRK_BLOCK,     BLOCK, RESET,
+        YELLOW_DRK_BLOCK,    BLOCK, RESET,
+        BLUE_DRK_BLOCK,      BLOCK, RESET,
+        MAG_DRK_BLOCK,       BLOCK, RESET,
+        CYAN_DRK_BLOCK,      BLOCK, RESET,
+        WHITE_LGHT_BLOCK,    BLOCK, RESET,
+        LGHT_BLOCK,          BLOCK, RESET,
+        RED_DRK_BLOCK,       BLOCK, RESET,
+        GREEN_LGHT_BLOCK,    BLOCK, RESET,
+        YELLOW_LGHT_BLOCK,   BLOCK, RESET,
+        BLUE_LGHT_BLOCK,     BLOCK, RESET,
+        MAG_LGHT_BLOCK,      BLOCK, RESET,
+        CYAN_LGHT_BLOCK,     BLOCK, RESET,
+        WHITE_DRK_BLOCK,     BLOCK, RESET
 };
 
 void Colourbar(const int rot) { 
@@ -32,7 +32,7 @@ void Colourbar(const int rot) {
         if (i % rot == 0) {
             fprintf(stdout, "%c", '\n');
         }
-        fprintf(stdout, "%s", *(BAR_ARRAY + i));
+        fprintf(stdout, "%s", *(BLOCK_ARRAY + i));
     }
     fprintf(stdout, "%c", '\n');
 }
--- include/colourbar/colourbar.h
+++ include/colourbar/colourbar.h
@@ -1,42 +1,42 @@
-#ifndef _COLOURBAR_H
-#define _COLOURBAR_H
+#ifndef _COLOURBLOCK_H
+#define _COLOURBLOCK_H
 
 #define MAX_COLOUR_SIZE 48
 
 /* Unicode Characters */
-#define BAR             "\u2588\u2588\u2588"
-#define ARROW           "\033[95m\u2794\033[0m"
-#define CHECK           "[\033[92m\u2714\033[0m]"
+#define BLOCK           "\u2588\u2588\u2588"
+#define SEPERATOR       "\033[95m\u2794\033[0m"
+#define COMPLETE        "[\033[92m\u2714\033[0m]"
 
-/* BAR Colour definitions */
-#define LGHT_BAR        "\033[90;100m"
-#define DRK_BAR         "\033[30;40m"
+/* BLOCK Colour definitions */
+#define LGHT_BLOCK        "\033[90;100m"
+#define DRK_BLOCK         "\033[30;40m"
 
-#define RED_LGHT_BAR    "\033[31;41m"
-#define RED_DRK_BAR     "\033[91;101m"
+#define RED_LGHT_BLOCK    "\033[31;41m"
+#define RED_DRK_BLOCK     "\033[91;101m"
 
-#define GREEN_LGHT_BAR  "\033[92;102m"
-#define GREEN_DRK_BAR   "\033[32;42m"
+#define GREEN_LGHT_BLOCK  "\033[92;102m"
+#define GREEN_DRK_BLOCK   "\033[32;42m"
 
-#define YELLOW_LGHT_BAR "\033[93;103m"
-#define YELLOW_DRK_BAR  "\033[33;43m"
+#define YELLOW_LGHT_BLOCK "\033[93;103m"
+#define YELLOW_DRK_BLOCK  "\033[33;43m"
 
-#define BLUE_LGHT_BAR   "\033[94;104m"
-#define BLUE_DRK_BAR    "\033[34;44m"
+#define BLUE_LGHT_BLOCK   "\033[94;104m"
+#define BLUE_DRK_BLOCK    "\033[34;44m"
 
-#define MAG_LGHT_BAR    "\033[95;105m"
-#define MAG_DRK_BAR     "\033[35;45m"
+#define MAG_LGHT_BLOCK    "\033[95;105m"
+#define MAG_DRK_BLOCK     "\033[35;45m"
 
-#define CYAN_LGHT_BAR   "\033[96;106m"
-#define CYAN_DRK_BAR    "\033[36;46m"
+#define CYAN_LGHT_BLOCK   "\033[96;106m"
+#define CYAN_DRK_BLOCK    "\033[36;46m"
 
-#define WHITE_LGHT_BAR  "\033[37;47m"
-#define WHITE_DRK_BAR   "\033[97;107m"
+#define WHITE_LGHT_BLOCK  "\033[37;47m"
+#define WHITE_DRK_BLOCK   "\033[97;107m"
 
 #define BLINK           "\033[5m"
 #define RESET           "\033[0m"
 
-extern const char *BAR_ARRAY[MAX_COLOUR_SIZE];
+extern const char *BLOCK_ARRAY[MAX_COLOUR_SIZE];
 extern void Colourbar(const int rot);
 
-#endif /* _COLOURBAR_H */
+#endif /* _COLOURBLOCK_H */
