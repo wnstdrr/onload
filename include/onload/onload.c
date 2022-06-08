3b42883e10e8dce9fb5faa01d4f56d9f9a857158
wnstdrr <xandertedder7@gmail.com>
2022-06-08 14:53:31 -0400

    added pkgs, small refactor

--- include/onload/onload.c
+++ include/onload/onload.c
@@ -7,6 +7,45 @@
 #include "onload.h"
 #include "../colourbar/colourbar.h" 
 
+
+char *Packages(void) {
+    const int size = 0xa;
+    char *Packages = (char*)malloc(sizeof(char)*size);
+
+    // assumes debian distro for now **the easy way**
+    // will probably add support for others later but
+    // that gets tedious with all the package managers.
+    FILE *PackageStream = popen("dpkg -l | egrep -c '^ii' | tr -d '\n'", "r");
+    if (PackageStream == NULL) {
+        Packages[size] = '\0';
+    }
+
+    while (fgets(Packages, size, PackageStream) != NULL) {
+        Packages[size] = '\0';
+    }
+    pclose(PackageStream);
+    return Packages;
+}
+
+char *OnloadENV(const char *env) {
+    // load a custom environment variable
+    char * e = getenv(env);
+    if (e == NULL) {
+        return '\0';
+    }
+    return e;
+}
+
+char *Terminal(void) {
+    char *Term = OnloadENV("TERM");
+    return Term;
+}
+
+char *Desktop(void) {
+    char *Desk = OnloadENV("XDG_CURRENT_DESKTOP");
+    return Desk;
+}
+
 void OnloadSYS(_SYS_INFO *info) {
     time_t now = time(&now);
     struct tm *timeinfo = localtime(&now);
@@ -27,37 +66,31 @@ void OnloadSYS(_SYS_INFO *info) {
     char *Release    = uname_info . release;
     int ReleaseSize  = strlen(Release)+1;
 
-    char *TERM       = getenv("TERM");
-    char *DESKTOP    = getenv("XDG_CURRENT_DESKTOP"); 
+    char *terminal   = Terminal();
+    char *desktop    = Desktop(); 
+    char *packages   = Packages();
    
-    info -> Terminal = TERM     != NULL ? TERM              : "n/a";
-    info -> Desktop  = DESKTOP  != NULL ? DESKTOP           : "n/a";
+    info -> Packages = packages;
+    info -> Terminal = terminal != NULL ? terminal : "n/a";
+    info -> Desktop  = desktop  != NULL ? desktop  : "n/a";
     info -> Time     = timeinfo != NULL ? asctime(timeinfo) : "n/a";
    
     memcpy(info -> Time,   TimeBuffer,  TimeBufferSize);
     memcpy(info -> Kernel, Release, ReleaseSize);
 }
 
-char *OnloadENV(const char *env) {
-    // load a custom environment variable
-    char * e = getenv(env);
-    if (e == NULL) {
-        return '\0';
-    }
-    return e;
-}
-
 void OnloadResult(_SYS_INFO *info) {
     char *RESULT[MAX_RESULT_COL][MAX_RESULT_ROW] = {
         {COMPLETE, " Terminal ", SEPERATOR, " ",  info -> Terminal},
         {COMPLETE, " Kernel   ", SEPERATOR, " ",  info -> Kernel},
+        {COMPLETE, " Pkgs     ", SEPERATOR, " ",  info -> Packages},
         {COMPLETE, " Desktop  ", SEPERATOR, " ",  info -> Desktop},
         {COMPLETE, " Time     ", SEPERATOR, " ",  info -> Time}
     };
 
     size_t i, j;
-    for (j = 0; j < 4; j++) {
-        for (i = 0; i < 5; i++) {
+    for (j = 0; j < 5; j++) {
+        for (i = 0; i < MAX_RESULT_ROW; i++) {
             fprintf(stdout, "%s", RESULT[j][i]);
         }
         fprintf(stdout, "%c", '\n');
