99bae47301e2fe8c4c32d87834912c0151d0437e
wnstdrr <xandertedder7@gmail.com>
2022-06-11 10:04:30 -0400

    remove _distro_type enum, unused

--- include/onload/onload.h
+++ include/onload/onload.h
@@ -17,15 +17,7 @@ typedef struct _SYS_INFO {
     char *ipv4;
 } _SYS_INFO;
 
-enum _distro_type {
-    debian_linux = 1 << 0,
-    arch_linux   = 1 << 1,
-    void_linux   = 1 << 2,
-    free_bsd     = 1 << 3
-};
-
 extern char *RESULT[MAX_RESULT_COL][MAX_RESULT_ROW];
-
 extern char *Dist(void);
 extern char *DeviceAddress(void);
 extern char *Packages(void);
