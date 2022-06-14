2e8a0f3d5fb45b99dd2b7ba26c48de0a616b3b84
wnstdrr <xandertedder7@gmail.com>
2022-06-14 14:26:51 -0400

    updated main.c, reflects changes in onload.c/h

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include "include/colourbar/colourbar.h"
 #include "include/onload/onload.h"
 
-#include <stdlib.h>
-#include <stdio.h>
 int main(void) {
-    _sys_o *sys_o = (_sys_o*)malloc(sizeof(_sys_o));
+    _sys_o *sys_o = (_sys_o*)sys_dyn_O();
     sys_O(sys_o);
 
-    signed int bar = cbar(8);
+    signed int bar;
+    bar = cbar(8);
     if (bar < 0) {
-        perror("Bar index out of range: 1..17");
-        exit(1);
+        return 1;
     }
+
     output_O(sys_o);
-    free(sys_o);
+    free_O(sys_o);
     return 0;
 }
